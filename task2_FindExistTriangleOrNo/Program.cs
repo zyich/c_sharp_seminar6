fd6de3075a19bf8e32d5652dea4cf95d3d80dae7
zyich <zuilinho@yandex.ru>
2023-07-05 21:43:11 +0200

    task 2 triangle or no

---
+++ task2_FindExistTriangleOrNo/Program.cs
@@ -0,0 +1,37 @@
+﻿/* 
+Задача 40: Напишите программу, которая принимает
+ на вход три числа и проверяет, может 
+ ли существовать треугольник 
+ с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона 
+треугольника меньше суммы двух других сторон.
+(теорема о неравенстве треугольника : каждая сторона треугольника 
+меньше суммы двух других сторон)
+*/
+
+
+int GetNumber(string description){
+
+    int number;
+    Console.WriteLine($"{description} => ");
+
+    while(true){
+        string temp = Console.ReadLine();
+        if(int.TryParse(temp, out number)){
+            return number;
+        }
+        Console.Write($"This number {temp} is not correct. Try again. => ");
+    }
+    
+}
+int a = GetNumber("Input number: ");
+int b = GetNumber("Input number: ");
+int c = GetNumber("Input number: ");
+
+bool FindTriangle(int a, int b, int c){
+    if((a < b+c) && (b < a+c) && (c < a+b)) return true;
+    else return false;
+}
+
+string answer = FindTriangle(a, b, c) ? "Yes" : "No";
+Console.WriteLine(answer);
