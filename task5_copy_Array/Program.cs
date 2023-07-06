d8c14d9c7f6a2924e4fe22fbde8b2aec38ee9edf
zyich <zuilinho@yandex.ru>
2023-07-06 16:10:32 +0200

    task5 copy array

---
+++ task5_copy_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая будет создавать копию заданного 
+массива с помощью поэлементного копирования.
+*/
+
+int[] CreateArray(int size, int min, int max){
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++){
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int[] array = CreateArray(5, 0, 10);
+Console.WriteLine($"Our list [{string.Join(", ", array)}]");
+
+
+int[] CopyArray (int[] array){
+    int[] copyArray = new int[array.Length];
+    for(int i = 0; i < array.Length; i++){
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
+
+int[] copyArray = CopyArray(array);
+Console.WriteLine($"Copy list[{string.Join(", ", copyArray)}]");
